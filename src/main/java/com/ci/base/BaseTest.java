5d8d2e4f9bc49b63ef12ef6b144cfbdeb7fa1050
ajaydev7556 <ajayit.bharatparamedical@gmail.com>
2024-10-25 16:49:17 +0530

    open setup code implement

--- src/main/java/com/ci/base/BaseTest.java
+++ src/main/java/com/ci/base/BaseTest.java
@@ -1,5 +1,18 @@
 package com.ci.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+
 public class BaseTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void runSetup()
+	{
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
 
 }
--- src/test/java/com/ci/runner/Runner.java
+++ src/test/java/com/ci/runner/Runner.java
@@ -1,5 +1,14 @@
 package com.ci.runner;
 
+import org.testng.annotations.Test;
+
 public class Runner {
+
+	
+	@Test()
+	public void login()
+	{
+		
+	}
 
 }
